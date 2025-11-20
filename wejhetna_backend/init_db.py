58b3fd26acc5aa2add31f7afe2204b49a1cfde65
alaa mkawi <alaamk@ac.sce.ac.il>
2025-11-20 16:11:42 +0200

    map

--- wejhetna_backend/init_db.py
+++ wejhetna_backend/init_db.py
@@ -1,5 +1,4 @@
 from db import Base, engine
-from models import Business
 
 print("Creating tables...")
 Base.metadata.create_all(bind=engine)
--- wejhetna_backend/schemas.py
+++ wejhetna_backend/schemas.py
@@ -1,19 +1,2 @@
 # schemas.py
 from pydantic import BaseModel
-
-# סכימה להצגת עסק (GET)
-class BusinessRead(BaseModel):
-    id: int
-    name: str
-    category: str | None = None
-
-    class Config:
-        orm_mode = True
-
-
-# סכימה ליצירת עסק חדש (POST)
-class BusinessCreate(BaseModel):
-    name: str
-    category: str | None = None
-    latitude: float
-    longitude: float
