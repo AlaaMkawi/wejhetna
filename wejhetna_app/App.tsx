c41dab14f2cb312136b09ba7dc2ff95b1215fe12
lena abuabid <lenaab1@ac.sce.ac.il>
2025-12-01 19:24:54 +0200

    update

--- wejhetna_app/App.tsx
+++ wejhetna_app/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import "./src/i18n";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./src/i18n";
+
+import React, { useEffect} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 
+import { loadAppLanguage, changeAppLanguage } from "./src/i18n";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+
+  // Load saved language when app starts
+  useEffect(() => {
+    const loadLang = async () => {
+      const lang = await loadAppLanguage();
+      await changeAppLanguage(lang);
+    };
+    loadLang();
+  }, []);
+
+    return (
+    <I18nextProvider i18n={i18n}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </I18nextProvider>
   );
 }
---
+++ wejhetna_app/DEVELOPER_SETUP.md
@@ -0,0 +1,3 @@
+npm install
+npm install react-native-localize i18next react-i18next
+npm install @react-native-async-storage/async-storage
