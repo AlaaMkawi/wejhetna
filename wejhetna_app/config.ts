33f8fa03639d9ad6e837c866ace47f664ecee7e9
alaa mkawi <alaamk@ac.sce.ac.il>
2026-05-20 15:39:08 +0300

    m

--- wejhetna_app/config.ts
+++ wejhetna_app/config.ts
@@ -1,4 +1,4 @@
 
-export const API_BASE_URL = "https://hydrant-mutable-liqueur.ngrok-free.dev"
+export const API_BASE_URL = " https://bullfrog-strainer-alright.ngrok-free.dev"
 export const NEARBY_DRIVER_RADIUS_M = 3000;
 export const RIDE_STATUS_POLL_INTERVAL_MS = 6000;
